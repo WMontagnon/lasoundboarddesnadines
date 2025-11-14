ccd82f6568730594f6618d5bb2d85d5c5885bc1a
William Montagnon <w.montagnon@gmail.com>
2025-11-14 18:32:41 +0100

    add sounds

--- src/App.tsx
+++ src/App.tsx
@@ -8,8 +8,12 @@ import wrongAnswerSound from './assets/badAnswer.mp3'
 import creditSongSound from './assets/credits.mp3'
 import startFinalSound from './assets/startFinale.mp3'
 import goodAnswerFinaleSound from './assets/goodAnswerFinale.mp3'
+import repeatAnswerSound from './assets/repeatAnswerFinale.mp3'
 import bingoSound from './assets/bingso.mp3'
 import mouhahaSound from './assets/mouHAHA.mp3'
+import alertRaidSound from './assets/alertRaid.mp3'
+import alertSubSound from './assets/alertSub.mp3'
+import joseNoCeBuenoSound from './assets/joseNoCeBueno.mp3'
 
 import Sound from './components/Sound'
 
@@ -33,8 +37,12 @@ function App() {
           <Sound soundComponent={creditSongSound} soundName="Générique UFEO" volume={volume} />
           <Sound soundComponent={startFinalSound} soundName="Début Finale UFEO" volume={volume} />
           <Sound soundComponent={goodAnswerFinaleSound} soundName="Bonne Réponse Finale UFEO" volume={volume} />
+          <Sound soundComponent={repeatAnswerSound} soundName="Répétition de la réponse" volume={volume} />
           <Sound soundComponent={bingoSound} soundName="Générique Bingo" volume={volume} />
           <Sound soundComponent={mouhahaSound} soundName="Mouhaha" volume={volume} />
+          <Sound soundComponent={alertSubSound} soundName="Alerte Sub" volume={volume} />
+          <Sound soundComponent={alertRaidSound} soundName="Alerte Raid" volume={volume} />
+          <Sound soundComponent={joseNoCeBuenoSound} soundName="Jose no ce buen" volume={volume} />
         </div>
       </main>
     </>
